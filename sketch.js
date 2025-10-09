1ffe29c1a121c0e83a2e018d73cfea1d310d5d63
rcenti <vlt534@york.ac.uk>
2025-10-09 12:18:06 +0100

    added food

--- sketch.js
+++ sketch.js
@@ -1,35 +1,44 @@
 const GRID_SIZE = 20;
 let headX = 40;
 let headY = 40;
-//let foodX;
-//let foodY;
-//let numberOfCells = width / GRID_SIZE;
-//let randomCell = random(numberOfCells);
-//let randomCellFloor = floor(randomCell);
+let foodX;
+let foodY;
+let numberOfCells = 600 / GRID_SIZE;
+let randomCell ; 
+let randomCellFloor; 
 
 function setup() {
     createCanvas(600, 600);
-
-    //foodX = randomCellFloor * GRID_SIZE + GRID_SIZE / 2;
-    //foodY = randomCellFloor * GRID_SIZE + GRID_SIZE / 2;
+    randomCell = random(numberOfCells);
+    randomCellFloor = floor(randomCell);
+       foodX = randomCellFloor * GRID_SIZE + GRID_SIZE / 2;
+       foodY = randomCellFloor * GRID_SIZE + GRID_SIZE / 2;
     frameRate(7);
+    
 }
 
 function draw() {
     background(0);
+    snake();
+    ellipse(foodX, foodY, GRID_SIZE, GRID_SIZE);
+    }
+    
+function snake() {
     fill(255);
-    rect(headX, headY, GRID_SIZE, GRID_SIZE);
 
+    rect(headX, headY, GRID_SIZE, GRID_SIZE);
     headX += GRID_SIZE;
 
     if (headX >= width) {
         headX = width - GRID_SIZE;
     }
 
-    
-    }
-       
+}
         
+
+function food() {
+
+}
     
     
 
